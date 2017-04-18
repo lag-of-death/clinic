656a3f12783a5b29d80d5f9a9ec3883e63c3ca9e
maggot <mateuszwit21@gmail.com>
2017-04-18 13:32:13 +0200

    Rearrange files

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var app = express();
 
 app.set('port', process.env.PORT || 5000);
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.listen(app.get('port'), function () {
