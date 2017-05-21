a9aee978bfb1af802fcc640d08ce4ce8f7b09dcc
maggot <mateuszwit21@gmail.com>
2017-05-21 14:10:05 +0200

    Add endpoint for retrieving a single nurse data

--- src-server/nurses.js
+++ src-server/nurses.js
@@ -21,11 +21,16 @@ var nurses = [
 
 module.exports = require('express').Router()
     .get('/api/nurses', getNursesHandler)
+    .get('/api/nurses/:id', getNurseHandler)
     .delete('/api/nurses/:id', delNurseHandler)
     .post('/api/nurses', newNurseHandler);
 
 function getNursesHandler(req, res) {
     res.send(nurses);
+}
+
+function getNurseHandler(req, res) {
+    res.send(nurses.find(nurse => nurse.personalData.id === parseInt(req.params.id)));
 }
 
 function delNurseHandler(req, res) {
