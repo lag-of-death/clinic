c29f262efa4f3572e35e632b286a62be333c9328
Mateusz Wit <mateuszwit21@gmail.com>
2018-06-12 21:08:43 +0200

    Refactor the way visits endpoints are handled

--- src-server/visits.js
+++ src-server/visits.js
@@ -1,38 +1,18 @@
-const rxjs = require(`rxjs`);
-const db = require(`./db`);
-
+const db      = require(`./db`);
+const rxjs    = require(`rxjs`);
 const express = require(`express`);
 
+const { of, fromPromise, forkJoin } = rxjs.Observable;
+
+const delVisitSubject  = new rxjs.Subject();
+const getVisitSubject  = new rxjs.Subject();
+const newVisitSubject  = new rxjs.Subject();
 const getVisitsSubject = new rxjs.Subject();
-const delVisitSubject = new rxjs.Subject();
-const getVisitSubject = new rxjs.Subject();
-const newVisitSubject = new rxjs.Subject();
 
-
-getVisitsSubject.subscribe((args) => {
-  const [req, res] = args;
-
-  getVisitsHandler(req, res);
-});
-
-delVisitSubject.subscribe((args) => {
-  const [req, res] = args;
-
-  delVisitHandler(req, res);
-});
-
-getVisitSubject.subscribe((args) => {
-  const [req, res] = args;
-
-  getVisitsHandler(req, res);
-});
-
-newVisitSubject.subscribe((args) => {
-  const [req, res] = args;
-
-  newVisitHandler(req, res);
-});
-
+setUpDelStream(delVisitSubject);
+setUpGetStream(getVisitSubject);
+setUpGetStream(getVisitsSubject);
+setUpCreateStream(newVisitSubject);
 
 module.exports = express.Router()
                         .get(`/api/visits`, (req, res) => getVisitsSubject.next([req, res]))
@@ -40,20 +20,15 @@ module.exports = express.Router()
                         .get(`/api/visits/:id`, (req, res) => getVisitSubject.next([req, res]))
                         .post(`/api/visits`, (req, res) => newVisitSubject.next([req, res]));
 
-
 function delVisitHandler(req, res) {
-  rxjs.Observable
-        .fromPromise(
-            db.query(`delete from visit where id = $1`, [req.params.id]),
-        )
-        .subscribe(
-            () => res.redirect(`/visits`),
-            err => res.send(err),
-        );
+  return fromPromise(db.query(`delete from visit where id = $1`, [req.params.id]))
+        .flatMap(() => of({ res }))
+        .catch(err => of({ err, res }));
 }
 
 function newVisitHandler(req, res) {
   const { patientID, doctorID, nurseID, date, room } = req.body;
+
   const queryParams = [
     +new Date(date),
     patientID,
@@ -62,29 +37,32 @@ function newVisitHandler(req, res) {
     room,
   ];
 
+  const select = `
+    select count(*) 
+    from visit 
+    where ((patient_id = $1 or doctor_id = $2 or nurse_id = $3) 
+    and date = $5) 
+    or (room = $4 and date = $5)
+    `;
 
-  const selectAsPromise = db.query(`select count(*) from visit where ((patient_id = $1 or doctor_id = $2 or nurse_id = $3) and date = $5) or (room = $4 and date = $5)`, [patientID, doctorID, nurseID, room, +new Date(date)]);
-  const selectAsStream = rxjs.Observable.fromPromise(selectAsPromise);
+  const selectAsPromise = db.query(select, [patientID, doctorID, nurseID, room, +new Date(date)]);
+  const selectAsStream  = fromPromise(selectAsPromise);
 
-  selectAsStream
+  return selectAsStream
         .flatMap((data) => {
           if (data[0].count >= 1) {
-            throw `We are sorry, date or room for patient/nurse/doctor already reserved.`;
+            return of({ res, err: true });
           } else {
-            const insertAsPromise = db.query(`insert into visit (id, date, patient_id, doctor_id, nurse_id, room) values (nextval('visit_id_seq'), $1, $2, $3, $4, $5)`, queryParams);
+            const insert = `
+                 insert into visit (id, date, patient_id, doctor_id, nurse_id, room) 
+                 values (nextval('visit_id_seq'), $1, $2, $3, $4, $5)`;
 
-            return rxjs.Observable.fromPromise(insertAsPromise);
+            const insertAsPromise = db.query(insert, queryParams);
+
+            return fromPromise(insertAsPromise).flatMap(() => of({ res }));
           }
-        })
-        .subscribe(
-            () => res.json(`Appointment was made.`),
-            (err) => {
-              res.status(422);
-              res.json(err);
-            },
-        );
+        });
 }
-
 
 function getVisitsHandler(req, res) {
   const promise = db.query(`
@@ -92,9 +70,16 @@ function getVisitsHandler(req, res) {
     visit.id, 
     visit.room,
     date,
-    json_build_object('id', patient.id, 'personal', json_build_object('id', pp.id, 'name', pp.name, 'surname', pp.surname, 'email', pp.email)) as patient,
-    json_build_object('id', nurse.id, 'personal', json_build_object('id', np.id, 'name', np.name, 'surname', np.surname, 'email', np.email), 'district', nurse.is_district_nurse) as nurse,
-    json_build_object('id', doctor.id, 'personal', json_build_object('id', dp.id, 'name', dp.name, 'surname', dp.surname, 'email', dp.email), 'speciality', doctor.speciality) as doctor
+    json_build_object('id', patient.id, 'personal', 
+    json_build_object('id', pp.id, 'name', pp.name, 'surname', pp.surname, 'email', pp.email)) as patient,
+    
+    json_build_object('id', nurse.id, 'personal', 
+    json_build_object('id', np.id, 'name', np.name, 'surname', np.surname, 'email', np.email), 
+    'district', nurse.is_district_nurse) as nurse,
+    
+    json_build_object('id', doctor.id, 'personal', 
+    json_build_object('id', dp.id, 'name', dp.name, 'surname', dp.surname, 'email', dp.email), 
+    'speciality', doctor.speciality) as doctor
   
    from visit
    
@@ -111,17 +96,56 @@ function getVisitsHandler(req, res) {
    order by date
    `, [req.params.id]);
 
+  return forkJoin([
+    fromPromise(promise),
+    of(res),
+    of(req),
+  ]);
+}
 
-  rxjs.Observable
-        .fromPromise(promise)
+function setUpGetStream(stream) {
+  return stream
+        .flatMap(([req, res]) => getVisitsHandler(req, res))
         .subscribe(
             (data) => {
+              const req    = data[2];
+              const resp   = data[1];
+              const visits = data[0];
+
               if (req.params.id) {
-                res.send(data[0]);
+                resp.send(visits[0]);
               } else {
-                res.send(data);
+                resp.send(visits);
               }
             },
-            err => res.send(err),
+        );
+}
+
+function setUpDelStream(stream) {
+  return stream
+        .flatMap(([req, res]) => delVisitHandler(req, res))
+        .subscribe(
+            ({ res, err }) => {
+              if (err) {
+                res.send(err);
+              } else {
+                res.redirect(`/visits`);
+              }
+            },
+        );
+}
+
+function setUpCreateStream(stream) {
+  return stream
+        .flatMap(([req, res]) => newVisitHandler(req, res))
+        .subscribe(
+            ({ res, err }) => {
+              if (!err) {
+                res.json(`Appointment was made.`);
+              } else {
+                res.status(422);
+                res.json(`We are sorry, date or room for patient/nurse/doctor already reserved.`);
+              }
+            },
         );
 }
