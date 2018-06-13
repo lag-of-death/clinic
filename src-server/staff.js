8766a342bf45262993d7bd34ca16b7948976e292
Mateusz Wit <mateuszwit21@gmail.com>
2018-06-13 08:44:36 +0200

    Fix all staff view not showing latest changes to nurses / doctors

--- src-server/staff.js
+++ src-server/staff.js
@@ -1,35 +1,35 @@
-const rxjs = require(`rxjs`);
+const rxjs    = require(`rxjs`);
 const express = require(`express`);
 const request = require(`request`);
 
 const getStaffSubject = new rxjs.Subject();
-const get = rxjs.Observable.bindNodeCallback(request);
+const get             = rxjs.Observable.bindNodeCallback(request);
 
-const getNursesStream = get(`${process.env.HOSTNAME}/api/nurses`);
-const getDoctorsStream = get(`${process.env.HOSTNAME}/api/doctors`);
+const getNursesStream  = () => get(`${process.env.HOSTNAME}/api/nurses`);
+const getDoctorsStream = () => get(`${process.env.HOSTNAME}/api/doctors`);
 
-const nursesSteam = getNursesStream.map(data => data[1])
-                                   .map(JSON.parse)
-                                   .map(data => data.map(toPerson.bind(null, `nurse`)))
-                                   .timeout(5000)
-                                   .catch(() => rxjs.Observable.of(``));
+const nursesSteam = () => getNursesStream().map(data => data[1])
+                                           .map(JSON.parse)
+                                           .map(data => data.map(toPerson.bind(null, `nurse`)))
+                                           .timeout(5000)
+                                           .catch(() => rxjs.Observable.of(``));
 
-const doctorsStream = getDoctorsStream.map(data => data[1])
-                                      .map(JSON.parse)
-                                      .map(data => data.map(toPerson.bind(null, `doctor`)))
-                                      .timeout(5000)
-                                      .catch(() => rxjs.Observable.of(``));
+const doctorsStream = () => getDoctorsStream().map(data => data[1])
+                                              .map(JSON.parse)
+                                              .map(data => data.map(toPerson.bind(null, `doctor`)))
+                                              .timeout(5000)
+                                              .catch(() => rxjs.Observable.of(``));
 
 getStaffSubject
     .flatMap(res =>
         rxjs.Observable
-            .forkJoin(nursesSteam, doctorsStream, rxjs.Observable.of(res))
+            .forkJoin(nursesSteam(), doctorsStream(), rxjs.Observable.of(res))
             .catch(() => rxjs.Observable.of([`no data`, `no data`, res])),
     )
     .subscribe(
         (data) => {
-          const lastItemIdx = data.length - 1;
-          const resp = data[lastItemIdx];
+          const lastItemIdx      = data.length - 1;
+          const resp             = data[lastItemIdx];
           const nursesAndDoctors = data.splice(0, lastItemIdx);
 
           resp.send(nursesAndDoctors[0].concat(nursesAndDoctors[1]));
